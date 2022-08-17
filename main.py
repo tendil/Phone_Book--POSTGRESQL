eeb9a422c1d32dd6edd23fc4cdcb5e640a546a7c
Dima <mejta.diman@gmail.com>
2022-08-18 01:26:12 +0300

    add v1.1 on the TenDil dranch

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
+import sys
 class Human:
-    def __init__(self, full_name=None, address=None, phone_number=None, from_line=None):
+    def __init__(self, last_name=None, name=None, address=None, phone_number=None, from_line=None):
         if from_line is None:
-            self.full_name = full_name
+            self.last_name = last_name
+            self.name = name
             self.address = address
             self.phone_number = phone_number
         else:
-            self.full_name, self.address, self.phone_number = str(from_line).replace(" ", '').split("|")
+            self.last_name, self.name, self.address, self.phone_number = str(from_line).replace(" ", '').split("|")
 
         def __str__(self):
-            return 'Member("{}", "{}", "{}")'.format(self.last_name, self.name, self.phone_number)
+            return 'Human("{}", "{}", "{}", "{}")'.format(self.last_name, self.name, self.address, self.phone_number)
 
     def input_characters(self):
-        self.full_name = input("Enter full name: ").capitalize()
+        self.last_name = input("Enter last name: ").capitalize()
+        self.name = input("Enter name: ").capitalize()
         self.address = input("Enter address: ").capitalize()
         self.phone_number = input("Enter phone number: ")
 
     def __str__(self):
-        return 'Human --> "{}" - "{}" - {}'.format(self.full_name, self.address, self.phone_number)
-
+        return 'Human --> "{}" - "{}" - "{}" - {}'.format(self.last_name, self.name, self.address, self.phone_number)
 
 class Contacts:
 
@@ -25,47 +27,51 @@ class Contacts:
         with open('base.txt') as file:
             for line in file:
                 human = Human(from_line=line)
-                print(human)
-                if human.full_name == query:
+                if human.last_name == query:
                     return human
-
+                if human.phone_number == query:
+                    return human
     def add_human(self):
         h = Human()
         h.input_characters()
-        if c.find_human(query=(h.full_name, h.address)) is None:
+        if c.find_human(query=(h.last_name, h.name)) is None:
             f = open('base.txt', 'a')
-            f.write('{0:10} | {1:10} | {2}'.format(h.full_name, h.address, h.phone_number) + '\n')
-            print('\nContact {full_name} successfully added\n'.format(full_name=h.full_name))
+            f.write('{0:10} | {1:10} | {2:10} | {3}'.format(h.last_name, h.name, h.address, h.phone_number) + '\n')
+            print('\nContact {last_name} {name} successfully added.)\n'.format(last_name=h.last_name, name=h.name))
             f.close()
-        else:
-            print('This contact already exists.')
+        # else:
+        #     print('This contact already exists.')
 
     def deleted_contacts(self, query):
-        global human
-        objects = []
-        f = open('base.txt', 'r+')
-        for line in f.readline():
-            human = Human(from_line=line)
-            objects.append(human)
-        for object in objects:
-            if (human.full_name) != query:
-                f.write(object.__str__())
+        pass
+        # f = open('base.txt', 'r')
+        # line = f.readlines()
+        # f.close()
+        # f = open('base.txt', 'w')
+
+        # for line in f.readline():
+        #     human = Human(from_line=line)
+        #     objects.append(human)
+        # for object in objects:
+        #     if (human.last_name, human.name) != query:
+        #         f.write(object.__str__())
+
     def show_all_contacts(self):
         with open('base.txt') as file:
             for line in file:
                 human = Human(from_line=line)
-                print(human)
-
-
+                # result = enumerate(file.readlines())
+                print(f"{human}" + '-' * len(line))
 
 def choice():
     sel = None
     try:
-        sel = int(input('Search - "1"\n' + \
-                        'New contact - "2"\n' + \
-                        'Show all phone book - "3"\n' + \
-                        'Deleted contact - "4"\n' + \
-                        'Edit contact - "5"\n' + \
+        sel = int(input('Search - "1"\n'
+                        'New contact - "2"\n'
+                        'Show all phone book - "3"\n'
+                        'Deleted contact - "4"\n'
+                        'Edit contact - "5"\n'
+                        'EXIT - "0"\n'
                         '\nEnter --->: '))
     except ValueError:
         print('\n\nInvalid input!!!')
@@ -78,8 +84,11 @@ c = Contacts()
 while True:
     sel = choice()
     if sel == 1:
-        query = (input('To search for a contact, enter his full name: ').capitalize())
+        query = (input('To search for a contact, enter his full name or phone number: ').capitalize())
         print(c.find_human(query))
+
+    elif sel == 0:
+        sys.exit()
 
     elif sel == 2:
         c.add_human()
@@ -88,7 +97,7 @@ while True:
         c.show_all_contacts()
 
     elif sel == 4:
-        query = ((input('To delete a contact, enter his full name: ').capitalize()))
+        query = ((input('To delete a contact, enter his last name and name: ').capitalize()))
         c.deleted_contacts(query)
 
     elif sel == 5:
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import sys
 class Human:
     def __init__(self, last_name=None, name=None, address=None, phone_number=None, from_line=None):
         if from_line is None:
@@ -9,7 +10,7 @@ class Human:
             self.last_name, self.name, self.address, self.phone_number = str(from_line).replace(" ", '').split("|")
 
         def __str__(self):
-            return 'Member("{}", "{}", "{}", "{}")'.format(self.last_name, self.name, self.address, self.phone_number)
+            return 'Human("{}", "{}", "{}", "{}")'.format(self.last_name, self.name, self.address, self.phone_number)
 
     def input_characters(self):
         self.last_name = input("Enter last name: ").capitalize()
@@ -20,55 +21,57 @@ class Human:
     def __str__(self):
         return 'Human --> "{}" - "{}" - "{}" - {}'.format(self.last_name, self.name, self.address, self.phone_number)
 
-
 class Contacts:
 
     def find_human(self, query=None):
         with open('base.txt') as file:
             for line in file:
                 human = Human(from_line=line)
-                print(human)
                 if human.last_name == query:
                     return human
-
+                if human.phone_number == query:
+                    return human
     def add_human(self):
         h = Human()
         h.input_characters()
         if c.find_human(query=(h.last_name, h.name)) is None:
             f = open('base.txt', 'a')
-            f.write('{0:10} | {1:10} | {2} | {3}'.format(h.last_name, h.name, h.address, h.phone_number) + '\n')
-            print('\nContact {last_name} {name} successfully added\n'.format(last_name=h.last_name, name=h.name))
+            f.write('{0:10} | {1:10} | {2:10} | {3}'.format(h.last_name, h.name, h.address, h.phone_number) + '\n')
+            print('\nContact {last_name} {name} successfully added.)\n'.format(last_name=h.last_name, name=h.name))
             f.close()
         # else:
         #     print('This contact already exists.')
 
     def deleted_contacts(self, query):
-        global human
-        objects = []
-        f = open('base.txt', 'r+')
-        for line in f.readline():
-            human = Human(from_line=line)
-            objects.append(human)
-        for object in objects:
-            if (human.last_name, human.name) != query:
-                f.write(object.__str__())
+        pass
+        # f = open('base.txt', 'r')
+        # line = f.readlines()
+        # f.close()
+        # f = open('base.txt', 'w')
+
+        # for line in f.readline():
+        #     human = Human(from_line=line)
+        #     objects.append(human)
+        # for object in objects:
+        #     if (human.last_name, human.name) != query:
+        #         f.write(object.__str__())
 
     def show_all_contacts(self):
         with open('base.txt') as file:
             for line in file:
                 human = Human(from_line=line)
-                print(human)
-
-
+                # result = enumerate(file.readlines())
+                print(f"{human}" + '-' * len(line))
 
 def choice():
     sel = None
     try:
-        sel = int(input('Search - "1"\n' + \
-                        'New contact - "2"\n' + \
-                        'Show all phone book - "3"\n' + \
-                        'Deleted contact - "4"\n' + \
-                        'Edit contact - "5"\n' + \
+        sel = int(input('Search - "1"\n'
+                        'New contact - "2"\n'
+                        'Show all phone book - "3"\n'
+                        'Deleted contact - "4"\n'
+                        'Edit contact - "5"\n'
+                        'EXIT - "0"\n'
                         '\nEnter --->: '))
     except ValueError:
         print('\n\nInvalid input!!!')
@@ -81,8 +84,11 @@ c = Contacts()
 while True:
     sel = choice()
     if sel == 1:
-        query = (input('To search for a contact, enter his full name: ').capitalize())
+        query = (input('To search for a contact, enter his full name or phone number: ').capitalize())
         print(c.find_human(query))
+
+    elif sel == 0:
+        sys.exit()
 
     elif sel == 2:
         c.add_human()
