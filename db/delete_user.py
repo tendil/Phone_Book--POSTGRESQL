78722215e67481d741404944ed3b5027a34949f1
Vladimir <v.gorobets77@mail.ru>
2022-08-22 17:09:27 +0300

    добил пунтк 5

--- db/delete_user.py
+++ db/delete_user.py
@@ -6,3 +6,4 @@ def delete_user(query):
     user = User.query.where(User.fullname == query).one()
     db_session.delete(user)
     db_session.commit()
+    print(f'\nThe contact has been successfully deleted\n')
